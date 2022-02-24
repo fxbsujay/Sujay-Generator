ad31a82ed3bce1914e80de6a823c3caf21d9dc32
fanxb <fxbsujay@gmail.com>
2022-02-24 15:55:53 +0800

    消息

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,5 +1,5 @@
 const request = axios.create({
-    baseURL: 'http://localhost' // 基础路径
+    baseURL: 'http://localhost:8001' // 基础路径
 })
 
 // 请求拦截器
@@ -15,18 +15,29 @@ request.interceptors.request.use(
 // 响应拦截器
 request.interceptors.response.use(
     response => {
-        return response
+        const res = response.data
+        if (res.code !== 200) {
+            ElementPlus.ElMessage.error({
+                message: res.msg
+            });
+            return Promise.reject(new Error(res.msg || 'Error'))
+        } else {
+            return res.data
+        }
+
     },
     err => {
+        ElementPlus.ElMessage.error({
+            message: '操作失败'
+        });
         return Promise.reject(err)
     }
 )
 
 
-function verify(data) {
+function sourcePage() {
     return request({
-        url: '/auth/verify',
-        method: 'post',
-        data
+        url: '/source/page',
+        method: 'get'
     })
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,22 @@
 const App = {
     name: 'APP',
     setup() {
+
+        let list = []
+        sourcePage().then((res) => {
+            console.log(res)
+        }).catch(() => {});
+
         let message = 'AAAAAA'
         const activeIndex = Vue.ref('1')
         const handleSelect = (key = '', keyPath = []) => {
             console.log(key, keyPath)
-            verify({})
+            ElementPlus.ElMessage.success({
+                message: '已取消'
+            });
         }
         return {
+            list,
             message,
             activeIndex,
             handleSelect
