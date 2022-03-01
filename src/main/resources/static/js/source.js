96a5ff75c5de078d037a5edceb6c7777adf8d6be
fxbsujay <fxbsujay@gmail.com>
2022-03-02 00:18:10 +0800

    弹框

--- src/main/resources/static/js/source.js
+++ src/main/resources/static/js/source.js
@@ -11,6 +11,13 @@ const Source = {
                 page: 1,
                 limit: 10
             },
+            /**
+             *  查询列表
+             * @param page 页码
+             * @param total 总条数
+             * @param limit 条数
+             * @returns {Promise<void>}
+             */
             async getList(page = null, total = null, limit = null) {
                 if (page) {
                     data.listQuery.page = page
@@ -26,25 +33,52 @@ const Source = {
                     data.listLoading = false
                 }, 0.5 * 1000)
             },
+            /**
+             * 分页器方法
+             * @param page 页码
+             */
             handleCurrentChange(page) {
                 data.getList(page).then(r => {})
             },
+            /**
+             * 分页器方法
+             * @param val 条数
+             */
             handleSizeChange(val) {
                 data.getList(null, null, val).then(r => {})
             }
         })
 
+        /**
+         * 查询按钮
+         */
         const search = () => {
-            sourcePage().then((res) => {
-                data.list = res.list
-            }).catch(() => {});
+            data.listQuery.page = 1
+            data.getList().then(r => {})
         }
+
+        const dialogVisible = Vue.ref(false)
+
+        const handleClose = (done) => {
+            ElementPlus.ElMessageBox.confirm('Are you sure to close this dialog?')
+                .then(() => {
+                    done()
+                })
+                .catch(() => {
+                    // catch error
+                })
+        }
+
         Vue.onMounted(() => {
             data.getList(null, null, 10).then(r => {})
         })
+
+
         return {
             ...Vue.toRefs(data),
             search,
+            dialogVisible,
+            handleClose
         }
     }
 }
